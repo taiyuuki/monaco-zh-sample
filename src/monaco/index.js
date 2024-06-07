9dc239f21e193cd58483eaecd18ba98382ca616c
taiyuuki <taiyuuki@qq.com>
2024-06-07 21:18:44 +0800

    fix: change ts worker to html worker

--- src/monaco/index.js
+++ src/monaco/index.js
@@ -1,11 +1,11 @@
 import * as monaco from 'monaco-editor'
-import typescriptWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 
 function initMonaco() {
 
     self.MonacoEnvironment = {
         getWorker: function (_workerId, label) {
-            return new typescriptWorker({ name: label })
+            return new htmlWorker({ name: label })
         }
     }
     return monaco
